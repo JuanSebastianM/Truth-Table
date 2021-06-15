7ea14b7199138359868553bf7e0b774da4494733
Juan Sebastián M <cebo_052000@hotmail.com>
2021-06-14 19:40:05 -0500

    Updated files

--- index.js
+++ index.js
@@ -10,53 +10,67 @@ function showTime() {
     }
 }
 showTime();
-const result = document.getElementById("result");
+let result = document.getElementById("result");
 
-const transferCost = document.getElementById("cost")
+let transferCost = document.getElementById("cost");
+
+let transferAmount = document.getElementById("transferAmount");
+
+let clientBank = document.getElementById("cbank"); //The client's bank.
+
+let clientBalance = document.getElementById("balance"); //Current client's balance.
+
+let clientName = document.getElementById("caccount"); //Account's holder name.
+
+let targetBank = document.getElementById("tbank"); //Account the money will be transferred to.
+
+let targetAccount = document.getElementById("taccount"); //Target account's holder name.
+
+let totalCost = clientBalance.value + transferCost.value;
+
+
+let verifiedClient = false; //This is the verified "client" account name.
+let verifiedTarget = false; //This is the verified "target" account name.
+
 
 const saveButton = document.getElementById("save");
 saveButton.addEventListener("click", save)
 function save() {
-    const clientBank = document.getElementById("cbank"); //The client's bank.
-    //console.log(clientBank.value);
-    const clientBalance = document.getElementById("balance"); //Current client's balance.
-    //console.log(clientBalance.value);
-    const clientName = document.getElementById("caccount"); //Account's holder name.
-    //console.log(clientName.value);
-    const targetBank = document.getElementById("tbank"); //Account the money will be transferred to.
-    //console.log(targetBank.value);
-    const targetAccount = document.getElementById("taccount"); //Target account's holder name.
-    //console.log(targetAccount.value);
-    if ((hour >= 9 && hour < 12) || (hour >= 15 && hour < 20)) {
-        if (clientBank.value == targetBank.value) {
-            transferCost.value = 0;
-        } 
-        else {
-            transferCost.value = 100;
+    let verify = document.getElementById("verify");
+    if ((clientName.value == "Daniela Páez" ^ clientName.value == "Juan Mendoza") && (targetAccount.value == "Daniela Páez" ^ targetAccount.value == "Juan Mendoza")) {
+        verifiedClient = true;
+        verifiedTarget = true;
+        if (clientName.value == "Daniela Páez") {
+            clientBalance.value = 1005000;
         }
+        else if (clientName.value == "Juan Mendoza") {
+            clientBalance.value = 2000500;
+        }
+        if ((hour >= 9 && hour < 12) || (hour >= 15 && hour < 20)) {
+            if (clientBank.value == targetBank.value) {
+                transferCost.value = 0;
+            } 
+            else {
+                transferCost.value = 100;
+            }
+        }
+        else {
+            result.innerHTML = "<strong>WARNING:</strong> Remember you can only make transfers from 9:00 to 12:00 and 15:00 to 20:00. Come back later!";
+        }
+    
     }
     else {
-        result.innerHTML = "<strong>WARNING:</strong> Remember you can only make transfers from 9:00 to 12:00 and 15:00 to 20:00. Come back later!";
+        verify.innerHTML = "Cannot proceed to make a transfer: account is not verified. Transfer procedure disabled."
     }
 }
-const verifiedClient = "Daniela Páez"; //determines whether the client account is verified or not.
-const verifiedTarget = "Juan Sebastián Mendoza"; //determines whether the target account is verified or not.
 
-
-/*if ((hour >= 9 && hour < 12) || (hour >= 15 && hour < 20)) {
-    if (clientBank == targetBank) {
-        transferCost = 0;
-    } 
-    else {
-        transferCost = 100;
-    }
-    if (verifiedClient == true && verifiedDestination == true && clientBalance >= transferCost) {
-        allowTransfer();
+const transferButton = document.getElementById("transferButton");
+transferButton.addEventListener("click", transfer)
+function transfer() {
+    if (parseInt(totalCost) > parseInt(transferAmount.value)) {
+        result.innerHTML = "<hr />Dear " + clientName.value + ", the transfer has been made succesfully from your <strong>" + clientBank.value + "</strong> account to " + targetAccount.value + "'s <strong>" + targetBank.value + "</strong> account. We thank you for using our services.";
     }
     else {
-        denyTransfer();
+        result.innerHTML = "<hr />Dear " + clientName.value + ", the total cost of the transfer exceeds the current balance of your account.";
     }
 }
-else {
-    denyTransfer();
-}*/
